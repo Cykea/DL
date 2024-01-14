3055d3de7f5442969448b2fb5cfd88db57fa7f90
Cykea <155712020+Cykea@users.noreply.github.com>
2024-01-14 16:08:17 +0800

    Add files via upload

--- predict.py
+++ predict.py
@@ -5,24 +5,23 @@ import torch
 from PIL import Image
 from torchvision import transforms
 import matplotlib.pyplot as plt
-import torch.nn as nn
 
-from model import resnet34
+from model import AlexNet
 
 
 def main():
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
     data_transform = transforms.Compose(
-        [transforms.Resize((256, 256)),
-         # transforms.CenterCrop(224),
+        [transforms.Resize((224, 224)),
          transforms.ToTensor(),
-         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])                # 要和训练标准化参数一样
+         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
     # load image
-    img_path = r'D:\DEEPLEARNING\Deeplearning\Bijie_landslide_dataset\devide_data\df035.png'
+    img_path = "../tulip.jpg"
     assert os.path.exists(img_path), "file: '{}' dose not exist.".format(img_path)
     img = Image.open(img_path)
+
     plt.imshow(img)
     # [N, C, H, W]
     img = data_transform(img)
@@ -37,26 +36,22 @@ def main():
         class_indict = json.load(f)
 
     # create model
-    # model = resnet34()
-    # in_channel = model.fc.in_features
-    # model.fc = nn.Linear(in_channel, 2)             # 对应model中的fc全连接层，5为花分类的类别数
-    # model.to(device)                                # 若不用迁移学习则注销上述几行代码
-    model = resnet34(num_classes=2).to(device)
-    # load model weights
-    weights_path = r"D:\DEEPLEARNING\Deeplearning\Bijie_landslide_dataset/resNet34.pth"
-    assert os.path.exists(weights_path), "file: '{}' dose not exist.".format(weights_path)
-    model.load_state_dict(torch.load(weights_path, map_location=device))
+    model = AlexNet(num_classes=5).to(device)
 
-    # prediction
+    # load model weights
+    weights_path = "./AlexNet.pth"
+    assert os.path.exists(weights_path), "file: '{}' dose not exist.".format(weights_path)
+    model.load_state_dict(torch.load(weights_path))
+
     model.eval()
-    with torch.no_grad():                   # 不对损失梯度进行跟踪
+    with torch.no_grad():
         # predict class
-        output = torch.squeeze(model(img.to(device))).cpu()          # 将图片输入到模型当中 压缩batch维度
-        predict = torch.softmax(output, dim=0)               # softmax处理得到概率分布
-        predict_cla = torch.argmax(predict).numpy()            # argmax寻找最大值对应的索引
+        output = torch.squeeze(model(img.to(device))).cpu()
+        predict = torch.softmax(output, dim=0)
+        predict_cla = torch.argmax(predict).numpy()
 
     print_res = "class: {}   prob: {:.3}".format(class_indict[str(predict_cla)],
-                                                 predict[predict_cla].numpy())              # 打印类别信息 概率
+                                                 predict[predict_cla].numpy())
     plt.title(print_res)
     for i in range(len(predict)):
         print("class: {:10}   prob: {:.3}".format(class_indict[str(i)],
